2a9af64f28d5e502a438644daeb6479c94deed85
Mahamood <shaikmisba305@gmail.com>
2024-12-07 12:02:01 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class array
+{
+    public static void main(String args[])
+    {
+        int arr[]={1,2,3,4};
+        System.out.println(Arrays.toString(arr));
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+    }
+}
